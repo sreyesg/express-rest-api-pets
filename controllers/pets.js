3fc70ea24d1b5803409eade442b20cdd536e44f0
sreyesg <santiagoreyes.g@gmail.com>
2024-11-06 21:07:39 -0800

    Define show and delete route for pets controller

--- controllers/pets.js
+++ controllers/pets.js
@@ -20,12 +20,33 @@ router.get('/', async(req, res) => {
     try {
         const allPets = await Pet.find()
         res.status(202).json(allPets)
-        
+    
     } catch (error) {
         res.status(500).json({error: error.message})
     }
 })
 
+// show route
+router.get('/:petId', async(req,res) =>{
+    try {
+        const foundPet = await Pet.findById(req.params.petId)
+        // res.json(foundPet)
+        res.status(202).json(foundPet)
+    } catch (error) {   
+        res.status(504).json({error: error.message})
+    }
+})
+
+// Delete route
+router.delete('/:petId', async(req,res) =>{
+    try {
+        const deletedPet = await Pet.findByIdAndDelete(req.params.petId)
+        res.status(202).json(deletedPet)
+        
+    } catch (error) {
+        res.status(504).json({error: error.message})
+    }
+})
 
 module.exports = router
 
